ebf01b15a9f5a059735dcdf91f44b97be416be95
Oleksandr Makogin <sashamakohin@gmail.com>
2022-09-01 21:01:43 +0300

    My home work 8

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.menu__button-open'),
+    closeMenuBtn: document.querySelector('.menu__button-close'),
+    menu: document.querySelector('.menu'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
